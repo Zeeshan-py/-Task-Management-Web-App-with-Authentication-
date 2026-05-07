f7cde3e8f304c01c577c249b72e7f8278cabb612
Zeeshan-py <zeeshanahmad0159@gmail.com>
2026-05-07 15:38:39 +0500

    fix(backend): bypass npm using Procfile to eliminate red error logs, bind 0.0.0.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,10 @@ app.get("/", (req, res) => {
   res.send("WORKING");
 });
 
+// VERY IMPORTANT: Railway injects process.env.PORT. 
 const PORT = process.env.PORT || 8080;
 
-// Removed "0.0.0.0" because Railway health checks use IPv6
-app.listen(PORT, () => {
-  console.log(`SERVER STARTED ON ${PORT}`);
+// Railway recommends binding to 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`SERVER IS SUCCESSFULLY RUNNING DIRECTLY ON NODE PORT ${PORT}`);
 });
